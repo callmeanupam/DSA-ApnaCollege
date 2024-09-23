02e2f588f8f281d4a9edde8acf55e9f2d8978bcf
Anupam Shakya <theanupamshakya@gmail.com>
2024-09-23 13:37:34 +0530

    Updating

--- Array/loops.cpp
+++ Array/loops.cpp
@@ -2,18 +2,9 @@
 using namespace std;
 
 int main(){
-    // int marks[5] = {40,45,35,62,64};
+   
     int size = 5;
     int marks[size];
-    // double price[] = {45.6, 56.3, 65.2}; // 3
-    // marks[0] = 101;
-    // cout << marks[0] << endl;
-    // cout << marks[1] << endl;
-    // cout << marks[2] << endl;
-    // cout << marks[3] << endl;
-    // cout << marks[4] << endl;
-    // cout << marks[5] << endl;
-
     // Loops : 0 to size-1
     // calculate size of array: 
     // cout << sizeof(marks) / sizeof(int) << endl;
@@ -29,6 +20,5 @@ int main(){
     for (int i = 0; i < size; i++) {
         cout << marks[i] << endl;
     }
-
     return 0;
 }
